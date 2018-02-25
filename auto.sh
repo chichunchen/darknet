d811b5a1f12bbba8cc22507b5854ff33b3c9c86a
chichunchen <chichunchen844@gmail.com>
2018-02-25 00:26:32 -0500

    Tweak for dumping the object info

--- auto.sh
+++ auto.sh
@@ -8,6 +8,6 @@ do
 	echo "Processing $filename:"
 	command="./darknet detect cfg/yolo.cfg yolo.weights $1/$filename 1>>$2"
 	eval $command
-#	add_id=`echo x >> $2`
-#	eval $add_id
+	add_id=`echo x >> $2`
+	eval $add_id
 done
